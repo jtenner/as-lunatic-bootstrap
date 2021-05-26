4e5eb81b419e0048df40a185a39f511372432397
Joshua Tenner <tenner.joshua@gmail.com>
2021-05-25 20:33:51 -0400

    default imports

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,8 @@
 /// <reference path="../node_modules/as-lunatic/assembly/index.d.ts" />
 // The entry file of your WebAssembly module requires this path to obtain the lunatic types
+import { Channel } from "channel";
+import { Process } from "thread";
+import { TCPServer } from "net";
 
 // start method
 export function _start(): void {
--- assembly/test.ts
+++ assembly/test.ts
@@ -1,4 +1,6 @@
 import { Channel } from "channel";
+import { Process } from "thread";
+import { TCPServer } from "net";
 
 export function _start(): void {
   // run your tests in lunatic here
