8f7f3edb179dede9dbef4ac0e8634d5ea80c1e7c
Joshua Tenner <tenner.joshua@gmail.com>
2021-05-25 21:10:49 -0400

    add socket

--- assembly/index.ts
+++ assembly/index.ts
@@ -2,7 +2,7 @@
 // The entry file of your WebAssembly module requires this path to obtain the lunatic types
 import { Channel } from "channel";
 import { Process } from "thread";
-import { TCPServer } from "net";
+import { TCPServer, TCPSocket } from "net";
 
 // start method
 export function _start(): void {
