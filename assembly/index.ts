906bcaf315357ac0e489e9d557f31188c5a39d5c
Joshua Tenner <tenner.joshua@gmail.com>
2021-05-25 20:31:16 -0400

    Initial commit

---
+++ assembly/index.ts
@@ -0,0 +1,7 @@
+/// <reference path="../node_modules/as-lunatic/assembly/index.d.ts" />
+// The entry file of your WebAssembly module requires this path to obtain the lunatic types
+
+// start method
+export function _start(): void {
+
+}
---
+++ assembly/test.ts
@@ -0,0 +1,5 @@
+import { Channel } from "channel";
+
+export function _start(): void {
+  // run your tests in lunatic here
+}
